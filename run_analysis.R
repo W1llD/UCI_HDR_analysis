892c4afed7c17d7157bc955eecd744d995d7b652
W1llD <dai.y@live.cn>
2014-11-23 23:47:51 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,5 +22,6 @@ gsub("[()]","",features$V2) -> features$V2
 names(HAR) <- c("subject","lable",as.character(features$V2))
 rm(subject_test,y_test,X_test,subject_train,y_train,X_train,test,train)
 
-HAR[,c(1,2,grep('mean[^F]|std',names(HAR)))] %>% mutate(activity = as.character(activity_lables[lable,2])) %>% select(c(1,60,3:59))-> HAR
-HAR %>% group_by(activity,subject) %>% 
+HAR[,c(1,2,grep('mean[^F]|std',names(HAR)))] %>% mutate(activity = as.character(activity_lables[lable,2])) %>% select(c(1,60,3:59)) -> HAR
+HAR %>% group_by(activity,subject) %>% summarise_each(funs(mean)) %>% write.table("tidiedHAR.txt")
+
